bc499e1836e5302de370081b9c86ba00379e9508
SintyTrishina <senty.goldsun@gmail.com>
2024-10-26 11:40:36 +0300

    second commit

--- app/src/main/java/com/example/playlistmakerapp/MainActivity.kt
+++ app/src/main/java/com/example/playlistmakerapp/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.playlistmakerapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +14,30 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+
+        val button1 = findViewById<Button>(R.id.button1)
+
+        button1.setOnClickListener {
+            val intent1 = Intent(this, Search::class.java)
+            startActivity(intent1)
         }
+
+        val button2 = findViewById<Button>(R.id.button2)
+
+        button2.setOnClickListener {
+            val intent2 = Intent(this, Media::class.java)
+            startActivity(intent2)
+        }
+
+        val button3 = findViewById<Button>(R.id.button3)
+
+        button3.setOnClickListener {
+            val intent3 = Intent(this, SettingsActivity::class.java)
+            startActivity(intent3)
+        }
+
+
     }
 }
