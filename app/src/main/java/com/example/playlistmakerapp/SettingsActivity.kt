5fbbcc593a1a094d877c2247e4f3e5c0628f8c85
SintyTrishina <senty.goldsun@gmail.com>
2024-11-11 19:50:08 +0300

    9 sprint 2 task

--- app/src/main/java/com/example/playlistmakerapp/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmakerapp/SettingsActivity.kt
@@ -1,11 +1,15 @@
 package com.example.playlistmakerapp
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.Switch
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintSet.Layout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -19,5 +23,55 @@ class SettingsActivity : AppCompatActivity() {
         buttonBack.setOnClickListener {
             finish()
         }
+
+//        val switchCompat = findViewById<Switch>(R.id.switch_compat)
+//
+//        switchCompat.setOnCheckedChangeListener { buttonView, isChecked ->
+//            if (isChecked) {
+//                println("Switch включен")
+//            } else {
+//                println("Switch выключен")
+//            }
+//        }
+
+        val buttonShare = findViewById<FrameLayout>(R.id.buttonShare)
+
+        buttonShare.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            val message =
+                "Привет! Посмотри этот курс по Android-разработке в Практикуме: https://practicum.yandex.ru/android-developer/"
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+
+            startActivity(Intent.createChooser(intent, "Поделиться через"))
+        }
+
+        val buttonSupport = findViewById<FrameLayout>(R.id.buttonSupport)
+
+        buttonSupport.setOnClickListener {
+            intent.type = "text/plain"
+            val intent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("senty.goldsun@gmail.com"))
+            intent.putExtra(
+                Intent.EXTRA_SUBJECT,
+                "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            )
+            intent.putExtra(
+                Intent.EXTRA_TEXT,
+                "Спасибо разработчикам и разработчицам за крутое приложение!"
+            )
+            startActivity(Intent.createChooser(intent, "Выберите почтовый клиент"))
+        }
+
+
+        val buttonAgreement = findViewById<FrameLayout>(R.id.buttonAgreement)
+
+        buttonAgreement.setOnClickListener {
+            val intent =
+                Intent(Intent.ACTION_VIEW, Uri.parse("https://yandex.ru/legal/practicum_offer/"))
+            startActivity(intent)
+        }
+
+
     }
 }
