371bca000fe537151df50b1a496656512aba2f2d
SintyTrishina <senty.goldsun@gmail.com>
2024-11-14 10:29:02 +0300

    9 sprint

--- app/src/main/java/com/example/playlistmakerapp/Search.kt
+++ app/src/main/java/com/example/playlistmakerapp/Search.kt
@@ -14,24 +14,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
 
 class Search : AppCompatActivity() {
 
     private var userText: String = ""
+    private lateinit var inputEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        inputEditText = findViewById(R.id.inputEditText)
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
         val buttonBack = findViewById<ImageView>(R.id.back)
-
-        if (savedInstanceState != null) {
-            userText = savedInstanceState.getString(USER_TEXT, "")
-            inputEditText.setText(userText)
-        }
 
         buttonBack.setOnClickListener {
             finish()
@@ -50,7 +47,7 @@ class Search : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                clearButton.visibility = clearButtonVisibility(s)
+                clearButton.isVisible = !s.isNullOrEmpty()
             }
 
             override fun afterTextChanged(s: Editable?) {
@@ -70,12 +67,9 @@ class Search : AppCompatActivity() {
         const val USER_TEXT = "USER_TEXT"
     }
 
-    private fun clearButtonVisibility(s: CharSequence?): Int {
-        return if (s.isNullOrEmpty()) {
-            View.GONE
-        } else {
-            View.VISIBLE
-        }
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        userText = savedInstanceState.getString(USER_TEXT, "")
+        inputEditText.setText(userText)
     }
-
 }
