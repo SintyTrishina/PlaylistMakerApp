8db7aae2457e1d27a0717c12c63e6570fad48e82
SintyTrishina <senty.goldsun@gmail.com>
2024-12-10 13:52:28 +0300

    11 sprint hometask

--- app/src/main/java/com/example/playlistmakerapp/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmakerapp/TrackAdapter.kt
@@ -4,7 +4,13 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TrackAdapter(private val tracks: List<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
+class TrackAdapter(private var tracks: ArrayList<Track>) : RecyclerView.Adapter<TrackViewHolder>() {
+
+    fun updateTracks(newTracks: ArrayList<Track>) {
+        tracks.clear()
+        tracks.addAll(newTracks)
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.track_list, parent, false)
@@ -18,5 +24,6 @@ class TrackAdapter(private val tracks: List<Track>) : RecyclerView.Adapter<Track
     override fun onBindViewHolder(holder: TrackViewHolder, position: Int) {
         holder.bind(tracks[position])
     }
+
 
 }
