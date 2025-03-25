f9bc17302ba77388d2e75eec373fcbf61a023ccc
SintyTrishina <senty.goldsun@gmail.com>
2025-03-25 14:26:09 +0200

    17 sprint player

--- app/src/main/java/com/example/playlistmakerapp/util/App.kt
+++ app/src/main/java/com/example/playlistmakerapp/util/App.kt
@@ -3,6 +3,7 @@ package com.example.playlistmakerapp.util
 import android.app.Application
 import com.example.playlistmakerapp.main.domain.api.di.internalNavigationModule
 import com.example.playlistmakerapp.main.ui.di.mainViewModelModule
+import com.example.playlistmakerapp.player.ui.di.playerViewModelModule
 import com.example.playlistmakerapp.search.data.di.searchDataModule
 import com.example.playlistmakerapp.search.domain.di.interactorModule
 import com.example.playlistmakerapp.search.domain.di.trackRepositoryModule
@@ -12,34 +13,21 @@ import org.koin.core.context.GlobalContext.startKoin
 
 class App : Application() {
 
-//    private lateinit var sharedPref: SharedPreferences
-//    private var darkThemePrivate = false
-//    val darkTheme: Boolean
-//        get() = darkThemePrivate
 
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidContext(this@App)
-            modules(searchDataModule, searchViewModelModule, trackRepositoryModule, interactorModule, internalNavigationModule, mainViewModelModule)
+            modules(
+                searchDataModule,
+                searchViewModelModule,
+                trackRepositoryModule,
+                interactorModule,
+                internalNavigationModule,
+                mainViewModelModule,
+                playerViewModelModule
+            )
         }
         Creator.initialize(this)
-//        sharedPref = getSharedPreferences(Constants.THEME_SETTINGS, MODE_PRIVATE)
-//        darkThemePrivate = sharedPref.getBoolean(Constants.SWITCH_KEY, false)
-//        switchTheme(darkTheme)
     }
-//
-//    fun switchTheme(darkThemeEnabled: Boolean) {
-////        darkThemePrivate = darkThemeEnabled
-////        AppCompatDelegate.setDefaultNightMode(
-////            if (darkThemeEnabled) {
-////                AppCompatDelegate.MODE_NIGHT_YES
-////            } else {
-////                AppCompatDelegate.MODE_NIGHT_NO
-////            }
-////        )
-////        sharedPref.edit()
-////            .putBoolean(Constants.SWITCH_KEY, darkThemeEnabled)
-////            .apply()
-//    }
 }
