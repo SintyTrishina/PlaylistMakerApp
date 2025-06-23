1f05ca935797be244237554b2934fcc5084ee3c2
SintyTrishina <senty.goldsun@gmail.com>
2025-06-23 14:12:05 +0200

    20 исправила прогресс аудиоплеера

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,4 +62,5 @@ dependencies {
     implementation(libs.androidx.viewpager2)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.kotlinx.coroutines.android)
 }
